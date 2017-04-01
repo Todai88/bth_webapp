80a933ea7e156da7df3d207396287ca9280dad0c
Todai88 <joabaj88@gmail.com>
2017-04-01 13:46:27 +0100

    working on presentation

--- redovisa/platforms/android/assets/www/js/views/layout.js
+++ redovisa/platforms/android/assets/www/js/views/layout.js
@@ -4,13 +4,26 @@ var m = require("mithril");
 module.exports = {
     view: function(vnode) {
         return m("main", [
-            m("navbar", [
-                m("div.container", [
-                    m("h2.brand", "Joakim BK"),
-                    m("ul.nav", [
-                        m("li", [m("a", {href: "/", oncreate: m.route.link}, "Me")]),
-                        m("li", [m("a", {href: "/hobby", oncreate: m.route.link}, "Hobby")]),
-                        m("li", [m("a", {href: "/reports", oncreate: m.route.link}, "Reports")])
+            m("nav", {class: "navbar navbar-toggleable-xs navbar-light bg-faded"}, [
+                m("button", {class: "navbar-toggler navbar-toggler-right",
+                             type: "button", 'data-toggle': "collapse",
+                             'data-target': "#navbarSupportedContent", 'aria-controls':"navbarSupportedContent",
+                             'aria-expanded':"false", 'aria-label':"Toggle navigation"}
+                             , [
+                                 m("span", {class: "navbar-toggler-icon"})
+                             ]),
+                m("a", {class: "navbar-brand", href: "/", oncreate: m.route.link}, "Joakim BK"),
+                m("div", {class: "collapse navbar-collapse", id: "navbarSupportContent"}, [
+                    m("ul", {class: "navbar-nav"}, [
+                        m("li", {class: "nav-item"}, [
+                            m("a", {class: "nav-link", href: "/", oncreate: m.route.link}, "Me")
+                        ]),
+                        m("li", {class: "navbar-item"}, [
+                            m("a", {class: "nav-link", href: "/hobby", oncreate: m.route.link}, "Hob")
+                        ]),
+                        m("li", {class: "navbar-item"}, [
+                            m("a", {class: "nav-link", href: "/reports", oncreate: m.route.link}, "Reports")
+                        ])
                     ])
                 ])
             ]),
